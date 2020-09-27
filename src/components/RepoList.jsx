ba4ab307d28a2fb125341281792d6f6cf76ec9b8
kevinreber <kevinreber1@gmail.com>
2020-09-27 13:27:57 -0700

    setup base styling

--- src/components/RepoList.jsx
+++ src/components/RepoList.jsx
@@ -3,21 +3,34 @@ import React from 'react';
 import moment from 'moment';
 import PropTypes from 'prop-types';
 
+/** Components */
+import StarIcon from './Icons/StarIcon';
+import ForkIcon from './Icons/ForkIcon';
+
+/** Styles */
+import './styles/RepoList.css';
+
 function RepoList({ repositories }) {
 	const RepoCards = repositories.map((repo) => (
 		<li className="Repo-Card">
-			<h3>{repo.name}</h3>
-			<p>{repo.language}</p>
+			<h3 className="Repo-Title">{repo.name}</h3>
 			<p>{repo.description}</p>
-			<p>{repo.stargazers_count}</p>
-			<p>{repo.forks_count}</p>
-			<p>{moment(repo.created_at).calendar()}</p>
+			<p>Language: {repo.language}</p>
+			<p>Created: {moment(repo.created_at).calendar()}</p>
+			<p>
+				<StarIcon />
+				{repo.stargazers_count}
+			</p>
+			<p>
+				<ForkIcon />
+				{repo.forks_count}
+			</p>
 		</li>
 	));
 
 	return (
 		<div>
-			<ul>{RepoCards}</ul>
+			<ul className="Repo-List">{RepoCards}</ul>
 		</div>
 	);
 }
