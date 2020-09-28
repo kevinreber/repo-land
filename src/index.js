9798914f935b87addc612cd0a050d6d8ba4398ec
kevinreber <kevinreber1@gmail.com>
2020-09-27 18:30:01 -0700

    installed jquery & popper js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import * as serviceWorker from './serviceWorker';
 
 /** React Router */
