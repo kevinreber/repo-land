40bf9c8ab727c603152ff76ecec4de21a3b7cc0d
kevinreber <kevinreber1@gmail.com>
2020-09-29 12:32:50 -0700

    styles: added media queries for Header and Brand components

--- src/components/layout/Brand.jsx
+++ src/components/layout/Brand.jsx
@@ -4,15 +4,11 @@ import React from 'react';
 /** Components */
 import GitIcon from '../icons/GitIcon';
 
+/** Brand Component for Header */
 function Brand() {
 	return (
 		<div className="Brand">
 			<GitIcon />
-			<h5 className="Brand-Title device-xs">
-				Repo
-				<br />
-				Land
-			</h5>
 			<h5 className="Brand-Title device-s">Repo Land</h5>
 		</div>
 	);
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -8,12 +8,26 @@ import NewOrganizationForm from '../NewOrganizationForm';
 /** Styles */
 import './styles/Header.css';
 
-function Header({ avatar, organization, search, setSearch, searchForOrg }) {
+/** Header Component.
+ *
+ * @param {string}    avatar         URL of organizations avatar.
+ * @param {string}    organization   Name of organization.
+ * @param {string}    search         State value of search search bar
+ * @param {function}  setSearch      Changes state search
+ * @param {function}  searchForOrg   Callback that changes state of organization
+ */
+function Header({
+	avatar = null,
+	organization,
+	search,
+	setSearch,
+	searchForOrg,
+}) {
 	return (
 		<header className="Main-Header">
 			<Brand />
 			<div className="Main-Header__Center">
-				<img src={avatar} alt="Logo" />
+				{avatar ? <img src={avatar} alt="Logo" /> : ''}
 				<h2 className="organization">{organization}</h2>
 			</div>
 			<NewOrganizationForm
