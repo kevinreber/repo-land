8bf0f29468ee0559c47918fee19867dec51cb4f3
kevinreber <kevinreber1@gmail.com>
2020-09-28 22:54:39 -0700

    added filter barto search for repositories by name

---
+++ src/components/NoData.jsx
@@ -0,0 +1,21 @@
+/** Dependencies */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/** Styles */
+import './styles/NoData.css';
+
+/**
+ * Component that let's user know that no data/matches exists.
+ *
+ * @param {string} text
+ */
+function NoData({ text }) {
+	return <p className="font-italic p-3 No-Data">No {text}</p>;
+}
+
+NoData.propTypes = {
+	text: PropTypes.string.isRequired,
+};
+
+export default NoData;
