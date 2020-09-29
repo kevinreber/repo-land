63703898143108e3267d7c57f833c335f6ed4170
kevinreber <kevinreber1@gmail.com>
2020-09-28 22:55:05 -0700

    added filter barto search for repositories by name

--- src/pages/Repositories.jsx
+++ src/pages/Repositories.jsx
@@ -1,14 +1,20 @@
 /** Dependencies */
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
 /** Components & Helpers */
 import RepoList from '../components/RepoList';
+import NoData from '../components/NoData';
+
+/** Styles */
+import './styles/Repositories.css';
 
 /**
  * Home Page displaying list of organization's repositories sorted by star count.
  */
 function Repositories({ repositories }) {
 	const [sortedRepos, setSortedRepos] = useState([]);
+	const [filter, setFilter] = useState('');
 
 	useEffect(() => {
 		/** Sorts Repos by stargazers_count */
@@ -32,11 +38,46 @@ function Repositories({ repositories }) {
 		}
 	}, [repositories]);
 
+	let Body;
+
+	if (filter !== '' && sortedRepos.length !== 0) {
+		// filter repositories to display
+		const filteredRepos = sortedRepos.filter(
+			(repo) => repo.name.toLowerCase().indexOf(filter.toLowerCase()) > -1
+		);
+
+		Body =
+			filteredRepos.length > 0 ? (
+				<RepoList repositories={filteredRepos} />
+			) : (
+				<NoData text={'repositories match'} />
+			);
+	} else if (sortedRepos && sortedRepos.length !== 0) {
+		Body = <RepoList repositories={sortedRepos} />;
+	} else {
+		Body = <NoData text={'repositories'} />;
+	}
+
 	return (
 		<div className="Main-Body__Content">
-			<RepoList repositories={sortedRepos} />
+			<div className="Search-Bar">
+				<input
+					id="filter"
+					className="form-control"
+					type="text"
+					placeholder="Search repositories by name..."
+					value={filter}
+					onChange={(e) => setFilter(e.target.value)}
+				/>
+			</div>
+			{Body}
+			{/* <RepoList repositories={sortedRepos} /> */}
 		</div>
 	);
 }
+
+Repositories.propTypes = {
+	repositories: PropTypes.array,
+};
 
 export default Repositories;
