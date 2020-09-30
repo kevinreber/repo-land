d800111dce5ff5249261a60a29a0447e541f1692
kevinreber <kevinreber1@gmail.com>
2020-09-30 09:53:21 -0700

    added variant to error message

--- src/components/errorMessage/ErrorMessage.jsx
+++ src/components/errorMessage/ErrorMessage.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-function ErrorMessage({ status, error }) {
+function ErrorMessage({ status, error, variant }) {
 	return (
-		<div className="error">
+		<div className={`error ${variant === 'repo' ? 'repo' : ''}`}>
 			<p>Error {status}</p>
 			<p>Sorry, something went wrong</p>
 			<p>{error}</p>
