aaed1e07e82114966a289aa9178dae71ac4871c3
kevinreber <kevinreber1@gmail.com>
2020-09-29 21:29:21 -0700

    added comments

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 /** Pagination List
+ *
+ * @param {number}    currentPage	  Current Page number in pagination.
+ * @param {number}    perPage		  Number of elements to display per page in pagination.
+ * @param {number}    total           Total number of elements in repositories array.
+ * @param {function}  paginate        Sets state of current page in pagination.
  */
 function Pagination({ currentPage, perPage, total, paginate }) {
 	const pageNumbers = [];
