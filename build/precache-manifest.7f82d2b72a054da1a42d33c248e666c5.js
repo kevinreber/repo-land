4ae609f1cd3cfcc7fbf8a122ab97077a4653d832
kevinreber <kevinreber1@gmail.com>
2020-10-12 17:08:10 -0700

    added build directory

---
+++ build/precache-manifest.7f82d2b72a054da1a42d33c248e666c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eab84d2968e94feb3247cc5fa68e57f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ebc935ca36a8dccdbb9",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "82f2909980a5a270e3a5",
+    "url": "/static/css/main.7bc7fa72.chunk.css"
+  },
+  {
+    "revision": "1ebc935ca36a8dccdbb9",
+    "url": "/static/js/2.e81efcc7.chunk.js"
+  },
+  {
+    "revision": "3cf8e5de02a476ec9d42a34d1a0a559b",
+    "url": "/static/js/2.e81efcc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82f2909980a5a270e3a5",
+    "url": "/static/js/main.a89840f1.chunk.js"
+  },
+  {
+    "revision": "f4a9275bc71aba21659e",
+    "url": "/static/js/runtime-main.4cd2bae6.js"
+  }
+]);
